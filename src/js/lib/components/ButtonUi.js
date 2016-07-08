0f46e9fe9e82d83a41c6ec8eb5ce7cc792e0f449
Florian Schroedl Home <flo.schroedl@gmail.com>
2016-07-08 18:22:03 +0200

    Add forward backward jump functionality

--- src/js/lib/components/ButtonUi.js
+++ src/js/lib/components/ButtonUi.js
@@ -19,13 +19,24 @@ class ButtonUi {
     this.elements.backward = qs('backward');
     this.elements.togglePlay = qs('toggleplay');
 
-    this.elements.togglePlay.addEventListener('mousedown', evt => {
+    this.elements.togglePlay.addEventListener('mouseup', evt => {
       let isPaused = this.timeline.paused();
       this.timeline.paused(!isPaused);
       evt.target.innerHTML = isPaused ? 'pause' : 'play_arrow';
-    })
-  }
+    });
 
+    this.elements.forward.addEventListener('mouseup', evt => {
+      let progress = this.timeline.progress();
+      this.timeline.progress(progress + progress * 0.3);
+    });
+
+    this.elements.backward.addEventListener('mouseup', evt => {
+      let progress = this.timeline.progress();
+      this.timeline.progress(progress - progress * 0.3);
+    });
+
+
+  }
 
 }
 export default ButtonUi;
