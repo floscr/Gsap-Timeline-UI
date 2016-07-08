7ca987d3c37c5182e171357f2ba21a0714e23fbf
Florian Schroedl Home <flo.schroedl@gmail.com>
2016-07-08 14:42:51 +0200

    Add timeline component

--- src/js/lib/components/Timeline.js
+++ src/js/lib/components/Timeline.js
@@ -2,14 +2,21 @@ import TweenMax from 'TweenMax';
 
 class Timeline {
 
-  constructor(containerEl, activeTimeline) {
+  constructor(containerEl, activeTimeline, isPlaying = true) {
     this.progress = 0;
+    this.isPlaying = isPlaying;
+    this.wasPlaying = isPlaying;
     this.activeTimeline = activeTimeline;
     this.elements = {
       container: containerEl,
     };
 
     this.startScrubbing = this.startScrubbing.bind(this);
+    this.stopScrubbing = this.stopScrubbing.bind(this);
+    this.scrubTo = this.scrubTo.bind(this);
+
+    this.startX = 0;
+    this.currentX = 0;
 
     this.createUi();
     this.addEventListeners();
@@ -25,15 +32,30 @@ class Timeline {
     this.elements.container.addEventListener('mousedown', this.startScrubbing);
   }
 
+  scrubTo(evt) {
+
+  }
+
+  stopScrubbing(evt) {
+    document.body.removeEventListener('mouseup', this.stopScrubbing);
+    document.body.removeEventListener('mousemove', this.scrubTo);
+    this.elements.container.addEventListener('mousedown', this.startScrubbing);
+
+    document.body.style.cursor = 'default';
+    if (this.wasPlaying) {
+      this.activeTimeline.play();
+    }
+  }
+
   startScrubbing(evt) {
-    // $timeline.classList.add(timelineScrubbingClass);
-    // this.elements.container.removeEventListener('mousedown', e => this.startScrubbing(e), false);
-    console.log('Pause');
     this.elements.container.removeEventListener('mousedown', this.startScrubbing);
+    document.body.addEventListener('mouseup', this.stopScrubbing);
+    document.body.addEventListener('mousemove', this.scrubTo);
+    // $timeline.classList.add(timelineScrubbingClass);
     // this.elements.addEventListener('mouseup', stopScrubbing, false);
     document.body.style.cursor = 'ew-resize';
-    // wasPlaying = !tl.paused();
 
+    this.wasPlaying = !this.activeTimeline.paused();
     this.activeTimeline.pause();
     // mouseIsDown = true;
     // mouseMove(e);
