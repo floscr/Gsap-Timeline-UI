052110bfd85c100888b65358e633f856c0ebaf46
Florian Schroedl Home <flo.schroedl@gmail.com>
2016-07-08 13:37:08 +0200

    Set update loop

--- src/js/lib/GsapUi.js
+++ src/js/lib/GsapUi.js
@@ -8,6 +8,8 @@ export default class GsapUi {
       rootElement: document.body,
     };
 
+    this.elements = {};
+
     this.timelines = [];
 
     // Accept single and multiple timeline objects
@@ -21,13 +23,25 @@ export default class GsapUi {
     this.activeTimeline = this.timelines[0];
 
     this.createUi();
+    this.addEventListeners();
   }
 
   createUi() {
+    // Create container element from jade template
     let containerEl = document.createElement('div');
     containerEl.id = 'gsapui';
     containerEl.className = 'gsapui';
     containerEl.innerHTML = template();
     this.config.rootElement.appendChild(containerEl);
+    this.elements.container = containerEl;
   }
+
+  addEventListeners() {
+    this.activeTimeline.eventCallback('onUpdate', () => this.update());
+    // window.addEventListener('keydown', (e)=>this.checkKeyCode(e), false);
+  }
+
+  update() {
+  }
+
 };
