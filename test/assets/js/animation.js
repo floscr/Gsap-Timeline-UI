5ac46c48ec0f5bac4129e15f9226202c37ea851c
Florian Schroedl Home <flo.schroedl@gmail.com>
2016-07-08 12:48:46 +0200

    Add test files to repository

---
+++ test/assets/js/animation.js
@@ -0,0 +1,25 @@
+TweenLite.ticker.fps(60);
+
+var green = document.getElementById('green');
+
+var tl = new TimelineMax({
+  repeat: -1
+});
+
+tl
+  .from(green, 2, {
+    scale: 0.2,
+    autoAlpha: 0
+  })
+  .to(green, 2, {
+    x: "100%"
+  })
+  .to(green, 0.5, {
+    x: "0%"
+  })
+  .to(green, 1, {
+    autoAlpha: 0
+  })
+;
+
+new window.GsapUi();
