edb0ac161aa62a6d848eb057313f516ee266e300
Florian Schroedl Home <flo.schroedl@gmail.com>
2016-07-08 14:49:18 +0200

    Add returnElementOffset helper

---
+++ src/js/lib/utils/returnElementOffset.js
@@ -0,0 +1,13 @@
+export default function returnElementOffset(element) {
+  let xPosition = 0;
+  let yPosition = 0;
+  while (element) {
+    xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+    yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+    element = element.offsetParent;
+  }
+  return {
+    x: xPosition,
+    y: yPosition
+  };
+}
