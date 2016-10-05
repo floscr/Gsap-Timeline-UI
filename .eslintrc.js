d5ee2e64b3d4c79359b4f0e82c757225cdd8bdf2
Florian Schroedl Home <flo.schroedl@gmail.com>
2016-10-05 22:41:46 +0200

    Set no-unused-vars to warn, not error

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
+    'no-unused-vars': 1,
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
