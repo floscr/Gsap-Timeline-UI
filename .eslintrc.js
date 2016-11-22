8e5b317aea3cc340bd023aca1aaab0a7dc6fc53c
Florian Schroedl Home <flo.schroedl@gmail.com>
2016-11-22 14:17:23 +0100

    Copy over eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,22 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    'no-unused-vars': 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'semi': 0,
+    "spaced-comment": [2, "always", {"exceptions": ["/-"], "markers": ["/"]}],
+    'keyword-spacing': 0,
+    'no-unreachable': 0,
+    'no-new': 0,
+    'no-multiple-empty-lines': 0,
+    'padded-blocks': 0,
+    'no-unused-vars': 1,
+    'no-undef': 0,
   }
 }
