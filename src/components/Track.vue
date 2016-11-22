812a8baba61705518fa7414d93ca4189c8f5e19d
Florian Schroedl Home <flo.schroedl@gmail.com>
2016-11-22 22:19:46 +0100

    Started adding dragger

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -39,8 +39,16 @@
 </style>
 
 <template>
-  <div class="container">
-    <div class="track" v-bind:style="{ transform: trackScaleX }"></div>
+  <div
+    v-on:mousedown='handleMouseDown'
+    class="container"
+    >
+    <div
+      class="track"
+      v-bind:style="{ transform: trackScaleX }"
+      >
+      {{ value }}
+    </div>
     <div class="cursor"></div>
   </div>
 </template>
@@ -50,6 +58,62 @@ import { mapGetters } from 'vuex'
 import round from 'lodash/round'
 
 export default {
+
+  data () {
+    return {
+      isMouseDown: false,
+      initialMouse: null,
+      value: 0
+    }
+  },
+
+  mounted () {
+    this.elements.timeline.addEventListener('mouseover', () => { this.mouseIsOver = true; });
+    this.elements.timeline.addEventListener('mousemove', evt => { this.currentX = evt.clientX; });
+  },
+
+  methods: {
+
+    handleMouseDown (event) {
+      this.mousedown = true
+
+      // remember the initial mouse position when the scubbing started
+      this.initialMouse = {
+        x: event.clientX,
+        y: event.clientY
+      }
+
+      // remember the initial value
+      this.initialValue = this.value;
+
+      // register global event handlers because now we are not bound to the component anymore
+      document.addEventListener('mousemove', this.handleMouseMove)
+
+      // global mouse up listener
+      document.addEventListener('mouseup', this.handleMouseUp)
+    },
+
+    handleMouseUp (event) {
+      // disable scrubbing
+      this.mouseDown = false;
+
+      document.removeEventListener('mousemove', this.handleMouseMove)
+      document.removeEventListener('mouseup', this.handleMouseUp)
+    },
+
+
+    // the actual translation of mouse movement to value change…
+    handleMouseMove (event) {
+      if (this.mouseDown) {
+        var newValue = this.initialValue + ((event.clientX - this.initialMouse.x) * this.steps)
+
+        // constrain the value to the min/max
+        this.value = this.constrain(newValue, this.min, this.max, this.decimals);
+      }
+    },
+
+  },
+
   computed: {
     trackScaleX () {
       return `scaleX(${round(this.progress, 3)})`
@@ -59,5 +123,6 @@ export default {
       'progress',
     ]),
   },
+
 }
 </script>
